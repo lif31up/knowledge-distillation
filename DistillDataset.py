995b6dd28e958fa1ac35d6a66bd96ed0c75ed103
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-11-06 00:00:30 +0900

    Refactor dataset and config for CIFAR-10 distillation
    Updated DistillDataset to accept a representation function and revised its consolidation logic. Refactored config.py to use ResNet50 pretrained on CIFAR-10, added HuggingFace Hub integration, and set up CIFAR-10 datasets and transforms. Removed the Transformer model implementation from model/Transformer.py, leaving only torch imports.

--- DistillDataset.py
+++ DistillDataset.py
@@ -3,24 +3,32 @@ from tqdm import tqdm
 
 
 class DistillDataset(Dataset):
-  def __init__(self, dataset, config):
+  def __init__(self, dataset, represent):
     super(DistillDataset, self).__init__()
-    self.config = config
-    self.dataset = dataset
+    self.dataset, self.represent = dataset, represent
     self.x, self.y = list(), list()
     self.is_consolidate = False
   # __init__
 
-  def __len__(self): return len(self.x)
+  def __len__(self): return len(self.y)
   def __getitem__(self, item): return self.x[item], self.y[item]
 
   def consolidate(self):
     progression = tqdm(self.dataset)
     for feature, label in progression:
-      feature = self.config.transform(feature)
-      label = (self.config.represent(feature), label)
+      label = (self.represent(feature), label)
       self.x.append(feature)
       self.y.append(label)
     self.is_consolidate = True
+    return self
   # consolidate
 # DistillDataset
+
+if __name__ == "__main__":
+  from config import Config
+  config = Config()
+  trainset = DistillDataset(dataset=config.trainset, represent=config.represent).consolidate()
+  for feature, label in trainset:
+    print(f'{feature, label}')
+    break
+# if __name__ == "__main__"
--- config.py
+++ config.py
@@ -1,20 +1,16 @@
 import torch
-from torchvision.models import resnet101, ResNet101_Weights
 import torchvision as tv
+from torchvision.models import resnet50
+from huggingface_hub import hf_hub_download
+
 
 class Config:
   def __init__(self):
-    self.save_to = "your_path"
+    self.save_to = "./model.bin"
 
-    self.n_heads = 12
-    self.n_stacks = 1
-    self.n_hidden = 2
-    self.dim = 768
-    self.output_dim = 2
     self.bias = True
-
+    self.n_convs = True
     self.dropout = 0.1
-    self.attention_dropout = 0.1
     self.eps = 1e-3
     self.betas = (0.9, 0.98)
     self.epochs = 5
@@ -25,22 +21,29 @@ class Config:
     self.init_weights = init_weights
     self.temperature = 4.0
 
-    self.model = resnet101(weights=ResNet101_Weights.IMAGENET1K_V2)
-    self.transform = transform
+    self.repo_id = "edadaltocg/resnet50_cifar10"
+    self.load_from = "./pytorch_model.bin"
+    hf_hub_download(repo_id=self.repo_id, filename=self.load_from)
+
+    self.model = resnet50(weights=None)
+    num_ftrs = self.model.fc.in_features
+    self.model.fc = torch.nn.Linear(num_ftrs, 10)
+    self.model.load_state_dict(torch.load(self.load_from, map_location=lambda storage, loc: storage))
+
+    self.transform = tv.transforms.Compose([
+      tv.transforms.Resize((28, 28)),
+      tv.transforms.ToTensor(),
+      tv.transforms.Normalize(mean=[0.5], std=[0.5]),
+    ]) # transform
     self.represent = lambda x: softoward(x=x, model=self.model, temperature=self.temperature)
-    self.dummy = None
+
+    self.trainset = tv.datasets.CIFAR10(root='./data', train=True, download=True, transform=self.transform)
+    self.testset = tv.datasets.CIFAR10(root='./data', train=False, download=True, transform=self.transform)
   # __init__
 # Config
 
-transform = tv.transforms.Compose([
-  tv.transforms.Resize((28, 28)),
-  tv.transforms.Grayscale(num_output_channels=1),
-  tv.transforms.ToTensor(),
-  tv.transforms.Normalize(mean=[0.5], std=[0.5]),
-]) # transform
-
 def softoward(x, model, temperature):
-  with torch.no_grad(): pred = model(**x)
+  with torch.no_grad(): pred = model(x)
   return F.softmax(pred / temperature, dim=1)
 # softoward
 
@@ -50,12 +53,12 @@ def init_weights(m):
     if m.bias is not None: nn.init.zeros_(m.bias)
 # init_weights
 
+
 if __name__ == "__main__":
   config = Config()
-  textset, textset_for_test = get_textset()
-  dataset = DistillDataset(textset, config.tokenizer, config.model, config.dim)
-  for feature, (soft_target, hard_target) in dataset:
-    print(f'feature: {feature}, soft_target: {soft_target}, hard_target: {hard_target}')
-    break
-  #for feature, (soft_target, hard_target) in dataset:
-#if
+  dataset = torch.utils.data.DataLoader(config.trainset, batch_size=config.batch_size, shuffle=True)
+  for feature, label in dataset:
+    print(f'{config.model(feature)}')
+    continue
+  # for
+# if __name__ == "__main__":
